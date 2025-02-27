153a58c2fa103aed72c028b8dd10ad04181038c8
DarkorZ <jcristofer13@gmail.com>
2025-02-27 14:00:36 -0500

    Exercise Initialization current

---
+++ Initialization_Error_exercise.py
@@ -0,0 +1,8 @@
+def count_down(start_number):
+  current = start_number ##SOLVE: initialization current as the parameter of the function
+  while (current > 0):
+    print(current)
+    current -= 1
+  print("Zero!")
+
+count_down(3)
